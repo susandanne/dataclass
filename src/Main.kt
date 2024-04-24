fd3b5df636416a2f671f390e1845fa772a525785
HP <susandanne@gmail.com>
2024-04-25 01:32:17 +0600

    data updated and solved

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
- val user1=User(1,"suman","suman@suman.com");
+ val user1=User(1,"suman","suman@suman.com1");
  val user2=User(2,null,"suman@suman.com");
  val user3=User(3,"suman",null);
 println("email length :${getemailUserLength(user1)} ")
@@ -12,12 +12,15 @@ println("email or placeholder${getEmailorPlaceholder(user3)}")
 
 }
 data class User(val id: Int,val name:String?,val email:String?)
- fun getemailUserLength(user: User?):Int{
-     return 0
+ fun getemailUserLength(user: User):Int{
+  var user1=user.email?.length
+     return user1!!
  }
-fun printUserNameLength(user: User?){
-
+fun printUserNameLength(user: User){
+ var user1=user.name
+ println(user1)
 }
-fun getEmailorPlaceholder(user: User?):String{
-return ""
+fun getEmailorPlaceholder(user: User):String{
+ var user1=user.email ?: "  no email provided"
+return user1
 }
